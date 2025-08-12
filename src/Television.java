1adddf655c45cf3e20fc8c06c49ce7e000218a39
AncelotLion <ancelot.lion@gmail.com>
2025-08-12 19:37:38 +0300

    new branch "homework06"

--- src/Television.java
+++ src/Television.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
 public class Television { //параметры
     private String brand;
     private double size;
     private boolean isOn;
-    private int channel;
+    private int currentChannel;
+    private List<Channel> channels;
+
 
     public Television(String brand, double size) { //бренд и размер будем передавать в экземплярах
         this.brand = brand;
         this.size = size;
         this.isOn = false;
-        this.channel = 1;
+        this.currentChannel = 1;
+        this.channels = new ArrayList<>();
     }
+
     //геттеры
     public String getBrand() {
         return brand;
@@ -23,9 +32,15 @@ public class Television { //параметры
         return isOn;
     }
 
-    public int getChannel() {
-        return channel;
+    public int getCurrentChannelNumber() {
+        return currentChannel;
     }
+
+    public List<Channel> getChannels() {
+        return channels;
+    }
+
+
     //сеттеры
     public void setBrand(String brand) {
         this.brand = brand;
@@ -43,13 +58,66 @@ public class Television { //параметры
         isOn = false;
     }
 
-    public void changeChannel(int channel) {
-        if(isOn) this.channel = channel; //если телевизор включен - меняет канал
+    public void addChannel(Channel channel) {
+        channels.add(channel);
+        System.out.println("Добавлен канал: " + channel.getName());
     }
 
-    public void printInfo() { //вывод в консоль
-        String status = isOn ? "вкл" : "выкл";
-        System.out.println(brand + ", " + size + "\", " + status + ", канал " + channel);
+    public void switchChannel(int channelNumber) {
+        if (!isOn) {
+            System.out.println("Телевизор выключен. Включите его сначала.");
+            return;
+        }
+
+        boolean channelFound = false;
+        for (Channel channel : channels) {
+            if (channel.getNumber() == channelNumber) {
+                currentChannel = channelNumber;
+                channelFound = true;
+                System.out.println("Переключено на канал: " + channel.getName());
+                channel.showProgramInfo();
+                break;
+            }
+        }
+
+        if (!channelFound) {
+            System.out.println("Канал с номером " + channelNumber + " не найден");
+        }
     }
+
+    public Channel getCurrentChannel() {
+        for (Channel channel : channels) {
+            if (channel.getNumber() == currentChannel) {
+                return channel;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Television{" +
+                "isOn=" + isOn +
+                ", currentChannelNumber=" + currentChannel +
+                ", channels=" + channels +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Television that = (Television) o;
+        return isOn == that.isOn &&
+                currentChannel == that.currentChannel &&
+                Objects.equals(channels, that.channels);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isOn, currentChannel, channels);
+    }
+
+
 }
 
